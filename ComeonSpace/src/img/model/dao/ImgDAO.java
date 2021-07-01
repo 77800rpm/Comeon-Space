0ef19fe6e9520b59c819156563ed420e7482abaf
Jin Gwang <75484965+sandeman96@users.noreply.github.com>
2021-07-01 23:57:32 +0900

    ...

--- ComeonSpace/src/img/model/dao/ImgDAO.java
+++ ComeonSpace/src/img/model/dao/ImgDAO.java
@@ -271,20 +271,25 @@ public class ImgDAO {
 		return imgList;
 	}
 
-	public int insertReview(Connection conn, Img img) {
+	public int insertReview(Connection conn, ArrayList<Img> fileList) {
 		PreparedStatement pstmt = null;
 		int result = 0;
 		
 		String query = prop.getProperty("insertReview");
-		
 		try {
-			pstmt = conn.prepareStatement(query);
-			pstmt.setInt(1, img.getUserNum());
-			pstmt.setString(2, img.getImgOrigin());
-			pstmt.setString(3, img.getImgChange());
-			pstmt.setString(4, img.getImgPath());
-			
-			result = pstmt.executeUpdate();
+			for(int i = 0; i < fileList.size(); i++) {
+				Img img = fileList.get(i);
+				
+				pstmt = conn.prepareStatement(query);
+				
+				pstmt.setInt(1, img.getImgCategory());
+				pstmt.setInt(2, img.getUserNum());
+				pstmt.setString(3, img.getImgOrigin());
+				pstmt.setString(4, img.getImgChange());
+				pstmt.setString(5, img.getImgPath());
+				
+				result += pstmt.executeUpdate();
+			}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
--- ComeonSpace/src/review/controller/InsertReviewServlet.java
+++ ComeonSpace/src/review/controller/InsertReviewServlet.java
@@ -49,7 +49,7 @@ public class InsertReviewServlet extends HttpServlet {
 			
 			int maxSize = 1024*1024*10;
 			String root = request.getSession().getServletContext().getRealPath("/");
-			String savePath = root + "thumbnail_uploadFiles/";
+			String savePath = root + "/img_upload/";
 			
 			System.out.println(savePath);
 			
@@ -75,48 +75,34 @@ public class InsertReviewServlet extends HttpServlet {
 					originFiles.add(multipartRequest.getOriginalFileName(name));
 				}
 			}
-			ArrayList<Img> list = new ArrayList<Img>();
-			if(originFiles != null) {
-				for(int i = originFiles.size()-1; i >= 0; i--) {
-						Img img = new Img();
-						img.setImgPath(savePath);
-						img.setImgOrigin(originFiles.get(i));
-						img.setImgChange(saveFiles.get(i));
-						
-						if(i == 0) {
-							img.setImgLevel(1);
-						} else if(i == 1) {
-							img.setImgLevel(2);
-						}
-						
-						list.add(img);
-				}
+			ArrayList<Img> fileList = new ArrayList<Img>();
+			
+			for(int i = originFiles.size()-1; i >= 0; i--) {
+				Img img = new Img();
+				img.setImgPath(savePath);
+				img.setImgOrigin(originFiles.get(i));
+				img.setImgChange(saveFiles.get(i));
+				img.setImgCategory(3);
+				img.setUserNum(userNum);
+								
+				fileList.add(img);
 			}
 			
-			String buyerName = request.getParameter("buyerName");
-			String prodName = request.getParameter("prodName");
+			String buyerName = multipartRequest.getParameter("buyerName");
+			String prodName = multipartRequest.getParameter("prodName");
 
-			int prodNum = Integer.parseInt(request.getParameter("prodNum"));
-			int orderNum = Integer.parseInt(request.getParameter("orderNum"));
+			int prodNum = Integer.parseInt(multipartRequest.getParameter("prodNum"));
+			int orderNum = Integer.parseInt(multipartRequest.getParameter("orderNum"));
 			
 			String revContent = multipartRequest.getParameter("revContent");
 			String revTitle = multipartRequest.getParameter("revTitle");
 			
-			request.setAttribute("userNum", userNum);
-			request.setAttribute("buyerNic", buyerNic);
-			request.setAttribute("buyerName", buyerName);
-			request.setAttribute("prodName", prodName);
-//			request.setAttribute("revDate", revDate);
-			request.setAttribute("revContent", revContent);
-			request.setAttribute("revTitle", revTitle);
-			request.setAttribute("prodNum", prodNum);
-			request.setAttribute("orderNum", orderNum);
-			
+						
 			Review review = new Review(userNum, prodNum, orderNum, buyerName, buyerNic, revContent, revTitle, prodName);
 			
 			int result = new ReviewService().insertReview(review);
 			
-			request.getRequestDispatcher("/WEB-INF/views/mypage/myPageOrderList.jsp").forward(request, response);
+			response.sendRedirect("orderList.my");
 
 		}
 	}
